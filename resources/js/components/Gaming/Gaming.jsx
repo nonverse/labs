245a9a97c86ad7c63e1d50ad6358ce78ee61e60b
isuru <isuru2003a@gmail.com>
2024-02-12 18:17:28 +1100

    Navigate user to game page on click of game card

--- resources/js/components/Gaming/Gaming.jsx
+++ resources/js/components/Gaming/Gaming.jsx
@@ -2,14 +2,20 @@ import ContentContainer from "../ContentContainer.jsx";
 import CardLarge from "../../elements/CardLarge.jsx";
 import games from "./games.json"
 import bg from "../../../assets/images/minecraft-landscape.jpeg"
+import {useNavigate} from "react-router";
 
 const Gaming = () => {
+
+    const navigate = useNavigate()
 
     return (
         <ContentContainer heading="Gaming" subHeading="Nonverse and community made gaming projects">
             <div id="games">
                 {games.map(game => (
-                    <CardLarge className="game-card" key={`game-${game}`} image={bg}>
+                    <CardLarge className="game-card" id={`game-${game.key}`} key={`game-${game.key}`} image={bg}
+                               onClick={() => {
+                                   navigate(`/gaming${game.location}`)
+                               }}>
                         <h1>{game.title}</h1>
                         <h2>{game.description}</h2>
                     </CardLarge>
