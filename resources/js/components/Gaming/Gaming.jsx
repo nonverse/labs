c6f9bb7ec3fa88f59ca58f3d923d4f01bbf33b2d
isuru <isuru2003a@gmail.com>
2024-02-12 17:06:57 +1100

    Create gaming sectoin

--- resources/js/components/Gaming/Gaming.jsx
+++ resources/js/components/Gaming/Gaming.jsx
@@ -1,9 +1,18 @@
 import ContentContainer from "../ContentContainer.jsx";
+import CardLarge from "../../elements/CardLarge.jsx";
+import mcBg from "../../../assets/images/minecraft-landscape.jpeg"
 
 const Gaming = () => {
 
     return (
-        <ContentContainer heading="Gaming" subHeading="Nonverse and community made gaming projects"/>
+        <ContentContainer heading="Gaming" subHeading="Nonverse and community made gaming projects">
+            <div id="games">
+                <CardLarge className="game-card" image={mcBg}>
+                    <h1>Minecraft Server</h1>
+                    <h2>The official Nonverse Minecraft server (Experiment)</h2>
+                </CardLarge>
+            </div>
+        </ContentContainer>
     )
 }
 
